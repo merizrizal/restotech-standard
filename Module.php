7bc39520d0cf42dc60876a8b20bc9b0b6d5387fb
merizrizal <meriz.rizal@gmail.com>
2018-01-18 13:10:12 +0700

    Commit

--- Module.php
+++ Module.php
@@ -8,5 +8,10 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+
+	$modules = [];
+        $modules['backend']['class'] = 'restotech\standard\backend\BackendModule';
+        $modules['frontend']['class'] = 'restotech\standard\frontend\FrontendModule';
+        $this->setModules($modules);
     }
 }
