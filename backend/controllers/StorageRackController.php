e2e1b7585dd6c8b9fda0583ae93221f27d91516c
merizrizal <meriz.rizal@gmail.com>
2018-01-23 19:27:28 +0700

    Commit

--- backend/controllers/StorageRackController.php
+++ backend/controllers/StorageRackController.php
@@ -4,12 +4,9 @@ namespace restotech\standard\backend\controllers;
 
 use Yii;
 use restotech\standard\backend\models\StorageRack;
-use restotech\standard\backend\models\search\StorageRackSearch;
 
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use yii\web\Response;
-use yii\widgets\ActiveForm;
 
 /**
  * StorageRackController implements the CRUD actions for StorageRack model.
@@ -24,159 +21,12 @@ class StorageRackController extends BackendController
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-                        'delete' => ['post'],
+                        
                     ],
                 ],
             ]);
     }
-
-    /**
-     * Lists all StorageRack models.
-     * @return mixed
-     */
-    public function actionIndex($sid)
-    {        
-        $searchModel = new StorageRackSearch();
-        $searchModel->storage_id = $sid;
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single StorageRack model.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new StorageRack model.
-     * If creation is successful, the browser will be redirected to the 'update' page.
-     * @return mixed
-     */
-    public function actionCreate($sid)
-    {
-        $model = new StorageRack();
-        $model->storage_id = $sid;
-        
-        if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
-            Yii::$app->response->format = Response::FORMAT_JSON;            
-            return ActiveForm::validate($model);
-        }
-
-        if ($model->load(Yii::$app->request->post())) {                        
-            
-            if ($model->save()) {
-                Yii::$app->session->setFlash('status', 'success');
-                Yii::$app->session->setFlash('message1', 'Tambah Data Sukses');
-                Yii::$app->session->setFlash('message2', 'Proses tambah data sukses. Data telah berhasil disimpan.');
-                
-                return $this->redirect(['update', 'id' => $model->id]);
-            } else {
-                $model->setIsNewRecord(true);
-                
-                Yii::$app->session->setFlash('status', 'danger');
-                Yii::$app->session->setFlash('message1', 'Tambah Data Gagal');
-                Yii::$app->session->setFlash('message2', 'Proses tambah data gagal. Data gagal disimpan.');
-            }
-        }
-         
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing StorageRack model.
-     * If update is successful, the browser will be redirected to the 'update' page.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-        
-        if (Yii::$app->request->isAjax && $model->load(Yii::$app->request->post())) {
-            Yii::$app->response->format = \Response::FORMAT_JSON;            
-            return ActiveForm::validate($model);
-        }
-
-        if ($model->load(Yii::$app->request->post())) {
-            if ($model->save()) {
-                Yii::$app->session->setFlash('status', 'success');
-                Yii::$app->session->setFlash('message1', 'Update Sukses');
-                Yii::$app->session->setFlash('message2', 'Proses update sukses. Data telah berhasil disimpan.');
-                
-                return $this->redirect(['update', 'id' => $model->id]);
-            } else {
-                Yii::$app->session->setFlash('status', 'danger');
-                Yii::$app->session->setFlash('message1', 'Update Gagal');
-                Yii::$app->session->setFlash('message2', 'Proses update gagal. Data gagal disimpan.');
-            }                        
-        }
-         
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing StorageRack model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        if (($model = $this->findModel($id)) !== false) {
-                        
-            $flag = false;
-            $error = '';
-            
-            try {
-                $flag = $model->delete();
-            } catch (yii\db\Exception $exc) {
-                $error = Yii::$app->params['errMysql'][$exc->errorInfo[1]];
-            }
-        }
-        
-        if ($flag) {
-            Yii::$app->session->setFlash('status', 'success');
-            Yii::$app->session->setFlash('message1', 'Delete Sukses');
-            Yii::$app->session->setFlash('message2', 'Proses delete sukses. Data telah berhasil dihapus.');            
-        } else {
-            Yii::$app->session->setFlash('status', 'danger');
-            Yii::$app->session->setFlash('message1', 'Delete Gagal');
-            Yii::$app->session->setFlash('message2', 'Proses delete gagal. Data gagal dihapus.' . $error);
-        }
-
-        return $this->redirect(['index', 'sid' => $model->storage_id]);
-    }
-        
-    public function actionGetStorageRack($id) 
-    {
-        $data = StorageRack::find()->where(['storage_id' => $id])->orderBy('nama_rak')->asArray()->all();
-        
-        $row = [];
-        
-        foreach ($data as $key => $value) {
-            $row[$key]['id'] = $value['id'];
-            $row[$key]['text'] = $value['nama_rak'];
-        }
-        
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        return $row;
-    }
-
+    
     /**
      * Finds the StorageRack model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
