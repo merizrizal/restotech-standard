d1898d08fe7163991323bb55f87e77f1453ff78b
Mei Rizal <meriz.rizal@gmail.com>
2020-07-09 06:23:49 +0700

    Fix kartikmpdf asset path

--- backend/controllers/SaleInvoiceController.php
+++ backend/controllers/SaleInvoiceController.php
@@ -93,7 +93,7 @@ class SaleInvoiceController extends BackendController
                     'orientation' => Pdf::ORIENT_PORTRAIT, 
                     'destination' => Pdf::DEST_DOWNLOAD, 
                     'content' => $content,  
-                    'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+                    'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
                     'cssInline' => file_get_contents(Yii::getAlias('@restotech/standard/backend/media/css/report.css')), 
                     'options' => ['title' => Yii::$app->name],
                     'methods' => [ 
